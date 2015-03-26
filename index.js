896e206ffa626cb90276e56c31d672c7fa3206fb
Justin <justincyork@gmail.com>
2015-03-26 20:12:31 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+var http = require('http'),
+    url = require('url'),
+    request = require('request'),
+    q = require('q'),
+    FamilySearch = require('familysearch-javascript-sdk');
+    
+http.createServer(function(req, res){
+
+    var requestUrl = url.parse(req.url, true);
+    console.log(requestUrl);
+    if(requestUrl.query && requestUrl.query.access_token && requestUrl.query.app_key){
+        var fs = new FamilySearch({
+            app_key: requestUrl.query.app_key,
+            environment: 'sandbox',
+            access_token: requestUrl.query.access_token,
+            http_function: request,
+            deferred_function: q.defer
+        });
+        
+        fs.getCurrentUser().then(function(userResponse){
+            res.writeHead(200);
+            res.write(userResponse.getUser().contactname);
+            res.end();
+        }, function(error){
+            res.writeHead(500);
+            res.write(error);
+            res.end();
+        });
+    }
+    
+    // 400 Bad Request
+    else {
+        res.writeHead(400);
+        res.write('Missing the app_key and/or the access_token query params');
+        res.end();
+    }
+    
+}).listen(process.env.PORT || 8080, process.env.IP);
